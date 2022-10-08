20da4ef6ddb51bdf737370b183001ea1ae19ab2b
facing-n <mike@nina.market>
2022-10-08 19:44:37 -0400

    initial

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  env: {
+    NINA_API_ENDPOINT: process.env.NINA_API_ENDPOINT,
+    NINA_PROGRAM_ID: process.env.NINA_PROGRAM_ID,
+    NINA_HUB_ID: process.env.NINA_HUB_ID,
+    SOLANA_CLUSTER_URL: process.env.SOLANA_CLUSTER_URL,
+  }
+}
